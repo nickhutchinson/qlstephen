cd26d06e22c4775f146a992b18e132b4ee943129
Nicholas Hutchinson <nshutchinson@gmail.com>
2012-09-02 15:46:14 +1200

    Replace NSRegularExpression with RegexKitLite for 10.6 support.
    TODO: Add credit for RegexKitLite, as its license requires.

--- QuickLookStephenProject/QLSFileAttributes.m
+++ QuickLookStephenProject/QLSFileAttributes.m
@@ -6,6 +6,7 @@
 //
 
 #import "QLSFileAttributes.h"
+#import "RegexKitLite.h"
 
 @interface QLSFileAttributes ()
 
@@ -22,34 +23,26 @@
 {
   NSString *magicString = [self magicStringForItemAtURL:aURL];
   if (!magicString) return nil;
-  
-  NSRegularExpression *magicRegex = self.magicOutputRegex;
-  
-  NSArray *matches = [magicRegex matchesInString:magicString
-                                         options:0
-                                           range:NSMakeRange(0, magicString.length)];
-  
+
+  NSArray *matches = [magicString rkl_captureComponentsMatchedByRegex:
+                         @"(\\S+/\\S+); charset=(\\S+)"];
+
   if (![matches count]) return nil;
-  
-  NSTextCheckingResult *regexResult = matches[0];
-  
-  NSRange mimeTypeRange = [regexResult rangeAtIndex:1];
-  NSRange charsetRange = [regexResult rangeAtIndex:2];
-  
-  NSString *mimetype = [magicString substringWithRange:mimeTypeRange];
-  NSString *charset = [magicString substringWithRange:charsetRange];
-  
-  BOOL mimeTypeIsTextual = [self mimeTypeIsTextual:mimetype];
-  
-  CFStringEncoding encoding = CFStringConvertIANACharSetNameToEncoding(
-                                  (CFStringRef)charset);
-  
+
+  NSString *mimeType = matches[1];
+  NSString *charset = matches[2];
+
+  BOOL mimeTypeIsTextual = [self mimeTypeIsTextual:mimeType];
+
+  CFStringEncoding encoding =
+      CFStringConvertIANACharSetNameToEncoding((CFStringRef)charset);
+
   QLSFileAttributes *attributes = [QLSFileAttributes new];
   attributes.fileEncoding = encoding;
   attributes.isTextFile = mimeTypeIsTextual;
-  attributes.mimeType = mimetype;
+  attributes.mimeType = mimeType;
   attributes.url = aURL;
-  
+
   return attributes;
 }
 
@@ -57,58 +50,42 @@
 // Private Methods
 ////////////////////////////////////////////////////////////////////////////////
 
-// FIXME: NSRegularExpression is not available on 10.6.
-+ (NSRegularExpression *)magicOutputRegex
-{
-  NSString *regexString =
-      @"(\\S+/\\S+); charset=(\\S+)";
-  
-  NSError *error;
-  NSRegularExpression *regex =
-      [NSRegularExpression regularExpressionWithPattern:regexString
-                                                options:0
-                                                  error:&error];
-  NSAssert(regex, @"Invalid regex: %@", error);
-  
-  return regex;
-}
-
 + (NSString *)magicStringForItemAtURL:(NSURL *)aURL
 {
   NSString *path = [aURL path];
   NSParameterAssert(path);
-  
+
   NSMutableDictionary *environment =
       [NSProcessInfo.processInfo.environment mutableCopy];
   environment[@"LC_ALL"] = @"en_US.UTF-8";
-  
+
   NSTask *task = [NSTask new];
   task.launchPath = @"/usr/bin/file";
   task.arguments = @[@"--mime", @"--brief", path];
   task.environment = environment;
   task.standardOutput = [NSPipe new];
-  
+
   [task launch];
-  
+
   NSData *output =
       [[task.standardOutput fileHandleForReading] readDataToEndOfFile];
 
   [task waitUntilExit];
-  
+
   if (task.terminationReason != NSTaskTerminationReasonExit
         || task.terminationStatus != 0) {
     return nil;
   }
-  
+
   NSCharacterSet *whitespaceCharset =
       [NSCharacterSet whitespaceAndNewlineCharacterSet];
-    
+
   NSString *stringOutput =
       [[NSString alloc] initWithData:output encoding:NSUTF8StringEncoding];
-  
+
   stringOutput =
       [stringOutput stringByTrimmingCharactersInSet:whitespaceCharset];
-  
+
   return stringOutput;
 }
 
@@ -122,20 +99,20 @@
   NSArray *components = [mimeType componentsSeparatedByString:@"/"];
   if (components.count != 2)
     return NO;
-  
+
   if ([components[0] rangeOfString:@"text"].location != NSNotFound)
     return YES;
-  
+
   NSString *UTType =
       CFBridgingRelease(UTTypeCreatePreferredIdentifierForTag(
                             kUTTagClassMIMEType,
                             (__bridge CFStringRef)mimeType,
                             kUTTypeData));
-  
+
   if (UTTypeConformsTo((__bridge CFStringRef)UTType, kUTTypeText)) {
     return YES;
   }
-  
+
   return NO;
 }
 
