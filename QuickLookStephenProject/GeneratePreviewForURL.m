f62314239dbddc1120bb3347f5f42223ed8e2a47
Nicholas Hutchinson <nshutchinson@gmail.com>
2012-09-02 15:50:54 +1200

    Cosmetic changes.
    - Tidy up some comments, indentation, etc.
    - Use Obj-C dot notation where appropriate.

--- QuickLookStephenProject/GeneratePreviewForURL.m
+++ QuickLookStephenProject/GeneratePreviewForURL.m
@@ -1,46 +1,50 @@
-#import <Foundation/Foundation.h>
+#include <CoreFoundation/CoreFoundation.h>
+#include <CoreServices/CoreServices.h>
+#include <QuickLook/QuickLook.h>
 
-#import <CoreFoundation/CoreFoundation.h>
-#import <CoreServices/CoreServices.h>
-#import <QuickLook/QuickLook.h>
+#import <Foundation/Foundation.h>
 
 #import "QLSFileAttributes.h"
 
 
 // Generate a preview for the document with the given url
-OSStatus GeneratePreviewForURL(void *thisInterface, QLPreviewRequestRef request,
-                               CFURLRef url, CFStringRef contentTypeUTI,
+OSStatus GeneratePreviewForURL(void *thisInterface,
+                               QLPreviewRequestRef request,
+                               CFURLRef url,
+                               CFStringRef contentTypeUTI,
                                CFDictionaryRef options) {
   @autoreleasepool {
     if (QLPreviewRequestIsCancelled(request))
       return noErr;
-    
-    QLSFileAttributes *magicAttributes
-        = [QLSFileAttributes attributesForItemAtURL:(__bridge NSURL *)url];
-    
+
+    QLSFileAttributes *magicAttributes =
+        [QLSFileAttributes attributesForItemAtURL:(__bridge NSURL *)url];
+
     if (!magicAttributes) {
       NSLog(@"QLStephen: Could not determine attribtues of file %@", url);
       return noErr;
     }
-    
+
     if (!magicAttributes.isTextFile) {
-//      NSLog(@"QLStephen: I don't think %@ is a text file", url);
       return noErr;
     }
-    
+
     if (magicAttributes.fileEncoding == kCFStringEncodingInvalidId) {
       NSLog(@"QLStephen: Could not determine encoding of file %@", url);
       return noErr;
     }
-    
+
     NSDictionary *previewProperties = @{
       (NSString *)kQLPreviewPropertyStringEncodingKey : @( magicAttributes.fileEncoding ),
       (NSString *)kQLPreviewPropertyWidthKey      : @700,
       (NSString *)kQLPreviewPropertyHeightKey     : @800
     };
-    
-    QLPreviewRequestSetURLRepresentation(request, url, kUTTypePlainText,
-                                         (__bridge CFDictionaryRef)previewProperties);
+
+    QLPreviewRequestSetURLRepresentation(
+        request,
+        url,
+        kUTTypePlainText,
+        (__bridge CFDictionaryRef)previewProperties);
 
     return noErr;
   }
--- QuickLookStephenProject/GenerateThumbnailForURL.m
+++ QuickLookStephenProject/GenerateThumbnailForURL.m
@@ -1,8 +1,8 @@
-#import <Foundation/Foundation.h>
+#include <CoreFoundation/CoreFoundation.h>
+#include <CoreServices/CoreServices.h>
+#include <QuickLook/QuickLook.h>
 
-#import <CoreFoundation/CoreFoundation.h>
-#import <CoreServices/CoreServices.h>
-#import <QuickLook/QuickLook.h>
+#import <Foundation/Foundation.h>
 
 #import "QLSFileAttributes.h"
 
@@ -14,63 +14,69 @@ static NSString *uppercaseString(NSString *aString, NSLocale *locale) {
 }
 
 
+/**
+ * This dictionary is used for a file with no extension. It maps the MIME type
+ * (as returned by file(1)) onto an appropriate thumbnail badge.
+ */
 static NSDictionary *MIMETypeToBadgeMap() {
   return @{
-      @"application/xml": @"xml",
-      @"text/x-c"       : @"C",
-      @"text/x-c++"     : @"C++",
-      @"text/x-shellscript" : @"shell",
-      @"text/x-php"     : @"php",
-      @"text/x-python"  : @"python",
-      @"text/x-perl"    : @"perl",
-      @"text/x-ruby"    : @"ruby"
+    @"application/xml": @"xml",
+    @"text/x-c"       : @"C",
+    @"text/x-c++"     : @"C++",
+    @"text/x-shellscript" : @"shell",
+    @"text/x-php"     : @"php",
+    @"text/x-python"  : @"python",
+    @"text/x-perl"    : @"perl",
+    @"text/x-ruby"    : @"ruby"
   };
 }
 
 /**
- * @return the text used to badge the thumbnail.
+ * @return the string that should be used to badge the thumbnail.
  */
-static NSString *ThumbnailBadgeForPlainTextItemAtURL(NSURL *url,
-                                                     NSString *mimeType) {
-  NSString *fileExtension = [url pathExtension];
+static NSString *ThumbnailBadgeForItemWithAttributes(
+    QLSFileAttributes *attributes) {
+  NSString *fileExtension = attributes.url.pathExtension;
   NSString *badge;
 
   // Do we have a file extension?
   if (![fileExtension isEqualToString:@""]) {
     badge = fileExtension;
-    
-    // Is the file extension too long to be reasonably displayed in a thumbnail?
-    // Is so, fall back on the additional tests.
-    
-    // FIXME1: use some better best to determine an appropriate length.
-    // FIXME2: perhaps truncate the extension (at the end? in the middle?) to
-    //         fit as much in the thumbnail as possible.
-    
-    if ([badge length] >= 10)
+
+    // Is the file extension too long to be reasonably displayed in a
+    // thumbnail? If so, fall back on the additional tests.
+
+    // FIXME: use some better test to determine an appropriate length.
+    // FIXME: perhaps we should truncate the extension (at the end? in the
+    //        middle?) to fit as much in the thumbnail as possible.
+
+    if (badge.length >= 10)
       badge = nil;
   }
 
   // Do we have a well-known MIME type? Note that we only do this test if we
-  // have no file extension. It's would be pretty jarring to get a misdiagnosis.
+  // have no file extension. file(1) might wrongly guess the MIME type, and it
+  // would be annoying if the file extension were to say one thing and the
+  // badge another.
   if (!badge && [fileExtension isEqualToString:@""]) {
     NSDictionary *map = MIMETypeToBadgeMap();
-    badge = map[mimeType];
+    badge = map[attributes.mimeType];
   }
-  
+
   // Do we have an executable text file? If so, assume it's a script of some
   // sort.
   if (!badge) {
     NSFileManager *fm = [NSFileManager new];
-    BOOL isExecutable = [fm isExecutableFileAtPath:[url path]];
+    BOOL isExecutable = [fm isExecutableFileAtPath:attributes.url.path];
     if (isExecutable)
       badge = @"script";
   }
-    
+
   if (!badge) {
     badge = @"txt"; // I would use "text", but the OS X text QuickLook
                     // generator uses "txt", and we ought to be consistent.
   }
-  
+
 
   return uppercaseString(badge, [NSLocale currentLocale]);
 }
@@ -78,61 +84,61 @@ static NSString *ThumbnailBadgeForPlainTextItemAtURL(NSURL *url,
 
 /* -----------------------------------------------------------------------------
  Generate a thumbnail for file
- 
+
  This function's job is to create thumbnail for designated file as fast as
  possible
  -------------------------------------------------------------------------- */
 OSStatus GenerateThumbnailForURL(void *thisInterface,
                                  QLThumbnailRequestRef request,
-                                 CFURLRef url, CFStringRef contentTypeUTI,
-                                 CFDictionaryRef options, CGSize maxSize) {
+                                 CFURLRef url,
+                                 CFStringRef contentTypeUTI,
+                                 CFDictionaryRef options,
+                                 CGSize maxSize) {
   @autoreleasepool {
     if (QLThumbnailRequestIsCancelled(request))
       return noErr;
-        
+
     QLSFileAttributes *magicAttributes
         = [QLSFileAttributes attributesForItemAtURL:(__bridge NSURL *)url];
-    
+
     if (!magicAttributes) {
       NSLog(@"QLStephen: Could not determine attribtues of file %@", url);
       return noErr;
     }
-    
+
     if (!magicAttributes.isTextFile) {
 //      NSLog(@"QLStephen: I don't think %@ is a text file", url);
       return noErr;
     }
-    
+
     if (magicAttributes.fileEncoding == kCFStringEncodingInvalidId) {
       NSLog(@"QLStephen: Could not determine encoding of file %@", url);
       return noErr;
     }
-    
+
     NSDictionary *previewProperties = @{
       (NSString *)kQLPreviewPropertyStringEncodingKey : @( magicAttributes.fileEncoding )
     };
-    
-    NSString *badge = ThumbnailBadgeForPlainTextItemAtURL(
-                          (__bridge NSURL *)url,
-                          magicAttributes.mimeType);
+
+    NSString *badge = ThumbnailBadgeForItemWithAttributes(magicAttributes);
 
     NSDictionary *properties = @{
       (NSString *)kQLThumbnailPropertyExtensionKey : badge
     };
-    
-    
+
+
     QLThumbnailRequestSetThumbnailWithURLRepresentation(
         request,
         url,
         kUTTypePlainText,
         (__bridge CFDictionaryRef)previewProperties,
         (__bridge CFDictionaryRef)properties);
-    
+
     return noErr;
   }
 }
 
-void CancelThumbnailGeneration(void* thisInterface, QLThumbnailRequestRef thumbnail)
-{
+void CancelThumbnailGeneration(void* thisInterface,
+                               QLThumbnailRequestRef thumbnail) {
     // implement only if supported
 }
